5d191df7f0d125e1e36e677286a1f282f9294ea2
George White <stonehippo@gmail.com>
2025-02-17 18:42:36 -0500

    Fixed some app flow

--- esppyflash/__main__.py
+++ esppyflash/__main__.py
@@ -1,5 +1,6 @@
 # a wrapper around esptool.py to simplify flashing MicroPython or CircuitPython onto an ESP32-C6 dev kit board
 import os
+import sys
 import click
 import subprocess
 import shlex
@@ -13,6 +14,13 @@ cp = "adafruit-circuitpython-espressif_esp32c6_devkitc_1_n8-en_US-9.2.4.bin"
               type=click.Choice(['MicroPython', 'CircuitPython']),
               help='Which version of Python to flash, e.g. MicroPython or CircuitPython')
 def cli(firmware):
+    click.echo("""IMPORTANT!!!
+       Before attempting to flash your ESP32 board, place it in bootloader\n
+       mode before continuing.\n
+       1. Press and hold the BOOT button.
+       2. Press and release the RESET button.
+       3. Release the BOOT button.\n""")
+    click.pause()
     app_path, _ = os.path.split(os.path.abspath(__file__))
     if firmware == 'CircuitPython':
         bin = cp
@@ -25,8 +33,9 @@ def cli(firmware):
         #subprocess.run(erase)
         click.echo(f"Flash erased!")
     else:
-        click.echo("Skipping erasing flash")
-    if click.confirm("Flash devive with {firmware}?"):
+        click.echo("Skipping erasing flash and exiting")
+        sys.exit()
+    if click.confirm(f"Flash device with {firmware}?"):
         click.echo(f"Flashing bin {bin} to device…")
         flash = shlex.split(f'esptool.py --baud 460800 write_flash 0 {bin_path}')
         #subprocess.run(flash)
